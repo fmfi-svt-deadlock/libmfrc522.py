b78a5649104380499eba6f13d8da4c04ef50a66d
Adam Dej <dejko.a@gmail.com>
2015-03-02 08:44:13 +0100

    Change version to a more descriptive number

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(name='libmfrc522',
       description='A library for communicating with the MFRC522 RFID module',
-      version='1.0',
+      version='0.1',
       url='https://github.com/fmfi-svt-gate/libmfrc522.py',
       license='MIT',
       packages=find_packages(),
