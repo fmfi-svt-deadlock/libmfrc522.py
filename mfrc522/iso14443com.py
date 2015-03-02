afe8c968b568c84477f06183b0b10b61b1a25854
Adam Dej <dejko.a@gmail.com>
2015-03-02 09:07:20 +0100

    Refactor: rewrite _perform_cascade function

--- mfrc522/iso14443com.py
+++ mfrc522/iso14443com.py
@@ -1,49 +1,66 @@
 import struct
+from enum import Enum
 import crcmod
 from .mfrc522 import MFRC522, Commands, Registers, NoTagError, TransmissionError
 
-SEL_CASCADE_1   = 0x93
-SEL_CASCADE_2   = 0x95
-SEL_CASCADE_3   = 0x97
-
 _crc_func = crcmod.mkCrcFun(0x11021, initCrc=0x6363)
+
+class CardCommands(Enum):
+    REQA         = 0x26
+    ANTICOLL_n   = [0x93, 0x95, 0x97]
+
+class CardException(Exception):
+    """The card did something unexpected."""
+    pass
 
 def _calculate_crc(data):
     return struct.pack('<H', _crc_func(data))
 
-def _perform_cascade(mfrc522, cascade_level):
-    if cascade_level != SEL_CASCADE_1 and \
-       cascade_level != SEL_CASCADE_2 and \
-       cascade_level != SEL_CASCADE_3:
-        return None
+def _perform_cascade(mfrc522):
+    result = b''
+    for cascade_level in CardCommands.ANTICOLL_n.value:
+        # transmit ANTICOLLISION command
+        uid_cln = mfrc522.transceive(bytes([cascade_level, 0x20]))
+        if len(uid_cln) != 5:
+            raise CardException
 
-    # transmit ANTICOLLISION command
-    uid_cln = mfrc522.transceive(bytes((cascade_level, 0x20)))
+        # transmit SELECT command
+        data = bytes((cascade_level, 0x70)) + bytes(uid_cln)
+        data += _calculate_crc(data)
+        response = mfrc522.transceive(data)
 
-    # transmit SELECT command
-    data = bytes((cascade_level, 0x70)) + bytes(uid_cln)
-    data += _calculate_crc(data)
-    response = mfrc522.transceive(data)
-
-    if response[0] & 0x04:
-        return uid_cln[1:4] + _perform_cascade(mfrc522, cascade_level + 2)
-    elif response[0] & 0x24 == 0x20:
-        return uid_cln[:4]
-    elif response[0] & 0x24 == 0:
-        return uid_cln[:4]
+        if response[0] & 0x24 == 0x00:
+            # UID complete, PICC is NOT compliant with ISO/IEC 14443-4
+            # TODO should we care?
+            result += uid_cln[:4]
+            return result
+        if response[0] & 0x24 == 0x20:
+            # UID complete, PICC compliant with ISO/IEC 14443-4
+            result += uid_cln[:4]
+            return result
+        elif response[0] & 0x04:
+            # UID incomplete; continue the cascade
+            result += uid_cln[1:4]
+            pass
+        else:
+            # Something's wrong if we get here.
+            raise CardException
+    # This cascade should always return UID, it should never end and get here.
+    raise CardException
 
 def get_id(mfrc522):
     mfrc522.write_register(Registers.BitFramingReg, 0x07)
-    mfrc522.transceive([0x26])  # REQA
+    mfrc522.transceive(CardCommands.REQA.value)
     mfrc522.write_register(Registers.BitFramingReg, 0x00)
-    return _perform_cascade(mfrc522, SEL_CASCADE_1)
+    return _perform_cascade(mfrc522)
 
 def are_cards_in_field(mfrc522):
     mfrc522.write_register(Registers.BitFramingReg, 0x07)
     try:
-        mfrc522.transceive([0x26])  # REQA
+        mfrc522.transceive(CardCommands.REQA.value)
         try:
-            mfrc522.transceive([0x26])  # REQA second time, to reset the card
+            # The second REQA command will reset the card
+            mfrc522.transceive(CardCommands.REQA.value)
         except NoTagError:
             # There should be no response, no modulation to the second REQA
             pass
